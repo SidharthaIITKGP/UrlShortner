6ae6693e93e730867727322b36fa7e0083775fde
SidharthaIITKGP <Sidharthaswain36@kgpian.iitkgp.ac.in>
2025-05-18 21:10:25 +0530

    Fix router and controller structure

--- routes/url.js
+++ routes/url.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {handleGenerateNewShortURL, handleGetAnalystics}= require('../controllers/url');
 router.post('/', handleGenerateNewShortURL);
-router.get('/analytics/:shortId', handleGetAnalystics);
+router.get('/analyconst express = require('express');
+const router = express.Router();
+const urlController = require('../controllers/url');
+
+router.post('/', urlController.handleGenerateNewShortURL);
+router.get('/analytics/:shortId', urlController.handleGetAnalystics);
+
+module.exports = router;tics/:shortId', handleGetAnalystics);
     
 module.exports = router;
